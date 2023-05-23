d67f254eb513d82844c52588c157ece7c169aaa2
Anup7870 <kumaranup2505@gmail.com>
2023-05-23 20:45:44 +0530

    routeChange

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
     
   },[]);
   return (!token ? (<Login />) : (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/musicPlayer'>
       <div className="mianBoady">
         <Sidebar />
         <Routes>
