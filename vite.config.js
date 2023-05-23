dec2def36fe5b6239cedc4decb93a346b80104c1
Anup7870 <kumaranup2505@gmail.com>
2023-05-23 20:39:40 +0530

    updating vite_config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/"
+  base:"/musicPlayer/"
 })
-if (command !== 'serve') {
-  config.base = '/react-vite-gh-pages/'
-}
+
