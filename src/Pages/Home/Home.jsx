97eab6e04b956f06968f4f8b673ed7109558c8b7
Anup7870 <kumaranup2505@gmail.com>
2023-05-24 20:56:24 +0530

    base to /

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
     
   },[]);
   return (!token ? (<Login />) : (
-    <BrowserRouter basename='.'>
+    <BrowserRouter basename='/'>
       <div className="mianBoady">
         <Sidebar />
         <Routes>
