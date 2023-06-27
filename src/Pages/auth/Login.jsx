b10aa2af83e7646480e97227dafbfa066cc939d7
Anup7870 <kumaranup2505@gmail.com>
2023-06-27 20:27:18 +0530

    error on image

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { loginEndpoint } from "../../spotify";
 import "./auth.scss";
+// import {required} from "browser"
+
 export default function Login() {
   return (
     <>
       <div className='Loginconatiner'>
         <div className='itemBox'>
-          <img src='src/assets/images.png' alt='' srcset='' />
+          <img src= 'src/assets/images.png' alt='' srcset='' />
           <a href={loginEndpoint}>
             <div className='buttonLogin'>Connect to Spotify</div>
           </a>
